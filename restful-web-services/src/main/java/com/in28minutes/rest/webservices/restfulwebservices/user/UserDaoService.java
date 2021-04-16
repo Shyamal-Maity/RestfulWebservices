1c2035e1414812fbfeb666b3595bc62d2cac2cbc
Shyamal Maity <65412412+Shyamal-Maity@users.noreply.github.com>
2021-04-16 13:17:54 +0530

    Add files via upload

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,57 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+
+	private static int userCount = 3;
+
+	private static List<User> users = new ArrayList<>();
+
+	static {
+		users.add(new User(1, "A", new Date()));
+		users.add(new User(2, "B", new Date()));
+		users.add(new User(3, "C", new Date()));
+	}
+
+	public List<User> findAll() {
+		return users;
+	}
+
+	public User save(User user) {
+		if (user.getId() == null) {
+			user.setId(++userCount);
+		}
+
+		users.add(user);
+		return user;
+	}
+
+	public User findOne(int id) {
+		for (User u : users) {
+			if (u.getId() == id) {
+				return u;
+			}
+		}
+		return null;
+	}
+
+	public User deleteById(int id) {
+		Iterator<User> iterator = users.iterator();
+		while (iterator.hasNext()) {
+			User user = iterator.next();
+			if (user.getId() == id) {
+				iterator.remove();
+				return user;
+			}
+		}
+		return null;
+	}
+
+}
